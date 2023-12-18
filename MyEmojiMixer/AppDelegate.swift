7ee43e7706ace5025fa4521e98093a7910de6be2
totoriole <89274127+totoriole@users.noreply.github.com>
2023-12-18 18:43:32 -0500

    Added support CoreData

--- MyEmojiMixer/AppDelegate.swift
+++ MyEmojiMixer/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -28,12 +29,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let nserror = error as NSError? {
+                print("Unresolved error \(nserror)" )
+            }
+        })
+        return container
+    }()
+
+    lazy var context: NSManagedObjectContext = {
+        persistentContainer.viewContext
+    }()
+    
+    func saveContext() {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                context.rollback()
+                let nserror = error as NSError
+                print("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
-
-
 }
 
